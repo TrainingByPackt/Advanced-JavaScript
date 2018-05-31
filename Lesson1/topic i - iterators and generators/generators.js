8429748f64e2379f9cc6c38eb4feaa42da50cf6c
DESKTOP-7S2VMF6\Zach <cosmonot1@gmail.com>
2018-05-31 01:28:43 -0700

    hints

--- Lesson1/topic i - iterators and generators/generators.js
+++ Lesson1/topic i - iterators and generators/generators.js
@@ -6,8 +6,9 @@ Create a generator that produces the fibonacci sequence (xn=xn-1+xn-2). If a tru
 is passed into the generator via next, then restart the sequence from seed n1=1 n0=0. If
 the value is over 10 then end generation. Start the sequence with the first two numbers
 (1 and 0) already saved in state.
-
  */
+
+// Solution
 
 function *fibonacci() {
   let n2 = 0;
@@ -37,3 +38,12 @@ console.log(gen.next().value);
 console.log(gen.next().value);
 console.log(gen.next().value);
 console.log(gen.next().value);
+
+/*
+Hints
+- Create a generator first that contains an infinite loop
+- Calculate and yield the next fibonacci number
+- Catch yield input with a variable and check truthyness
+- If truthy reset the seed values to 1 and 0
+- Update the seed values each step of the generator
+ */
