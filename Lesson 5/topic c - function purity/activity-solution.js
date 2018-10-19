78b5a2f25a6cee273f71c74b855d4cd81aba3828
Zach-home <cosmonot1@gmail.com>
2018-10-18 18:16:38 -0700

    first 2 activities for lesson 5

---
+++ Lesson 5/topic c - function purity/activity-solution.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//Activity C
+// You have been hired as a developer to upgrade an online store’s shopping cart implementation. Build a function to add items to the shopping cart. Your function should be pure. Create the function in the provided file (activity-test.js). The file will have basic tests to test purity.
+
+// Solution:
+function addItem( cart, item, quantity ) {
+  // Duplicate cart
+  const newCart = JSON.parse( JSON.stringify( cart ) );
+  newCart.push( ...Array( quantity ).fill( item ) );
+  return newCart;
+}
+
+/* Hints:
+- Create a function called addItem
+  - Takes in three parameters, the cart array, the item to add, and the quantity to add
+- Duplicate the cart to prevent side effects
+  - Use JSON operations for simplest duplication JSON.parse( JSON.stringify( cart ) )
+  - Can also step through original cart array with a loop and push each item to new array
+  - Can also use cart.map( () => {} ) since all items in array are simple types
+  - Can also use rest/spread operator newCart= [ ...cart ] because all items are simple types
+- Push the new items to the new cart array
+  - Use a for loop
+  - Use rest/spread operator and Array constructor with Array.fill()
+- Return the new cart array
+
+ */
---
+++ Lesson 5/topic c - function purity/activity-test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Write addItem function here
+function addItem() {}
+
+// Run this code to test your addItem function
+const cart = [ 'soap', 'toothpaste', 'toothpaste' ];
+const originalLength = cart.length;
+const quantityToAdd = 3;
+const cartModified = addItem( cart, 'carrot', quantityToAdd );
+
+if ( cart.length !== originalLength ) {
+  throw new Error( 'Original cart modified' );
+}
+
+if ( cartModified.length !== ( originalLength + quantityToAdd ) ) {
+  throw new Error( 'Did not add items to the new cart' );
+}
