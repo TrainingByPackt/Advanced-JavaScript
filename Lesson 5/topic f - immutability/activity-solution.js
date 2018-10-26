0d1d5c692c156da247cfcb1c32e6de23d654c852
Zach-home <cosmonot1@gmail.com>
2018-10-25 19:10:47 -0700

    fix missing hints

--- Lesson 5/topic f - immutability/activity-solution.js
+++ Lesson 5/topic f - immutability/activity-solution.js
@@ -9,5 +9,11 @@ function immutable( data ) {
 }
 
 /* Hints:
+- Create a function called immutable that takes in a single argument, data
+- If the type of data is not an object then return
+-- Use typeof !== ‘object’
+-- No need to freeze non objects
+- Freeze the data object
+- Loop through object values and recursively call the immutable function for each
 
  */
