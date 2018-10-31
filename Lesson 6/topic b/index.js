de76a5f979c9ecb83440074739432d5b4af2d42b
Zach-home <cosmonot1@gmail.com>
2018-10-30 23:27:04 -0700

    activity for topic a cmplete

--- Lesson 6/topic b/index.js
+++ Lesson 6/topic b/index.js
@@ -3,6 +3,8 @@
 const express = require( 'express' );
 const http = require( 'http' );
 const bodyParser = require( 'body-parser' );
+const fs = require( 'fs' );
+const notePath = './note.txt';
 
 console.log( 'Starting HTTP server.' );
 
@@ -12,20 +14,27 @@ const server = http.createServer( app );
 console.log( 'Configuring HTTP server.' );
 
 // Middleware
-app.use( bodyParser.json() );
+app.use( bodyParser.json( { strict: false } ) );
 
 // Route Handlers
 const router = express.Router();
-router.route( '/' ).get( ( req, res, next ) => {
-  console.log(req.body);
 
-  res.status( 200 ).json( 'nothing to see here' );
+router.route( '/' ).get( ( req, res ) => res.sendFile( 'index.html', { root: __dirname } ) );
+router.route( '/save' ).post( ( req, res ) => {
+  fs.writeFile( notePath, req.body, 'utf8', err => {
+    if ( err ) {
+      res.status( 500 );
+    }
+    res.end();
+  } );
 } );
-
-router.route( '/' ).post( ( req, res, next ) => {
-  console.log(req.body);
-
-  res.status( 200 ).json( 'nothing to see here' );
+router.route( '/load' ).get( ( req, res ) => {
+  fs.readFile( notePath, 'utf8', ( err, data ) => {
+    if ( err ) {
+      res.status( 500 ).end();
+    }
+    res.json( data );
+  } );
 } );
 
 app.use( '/', router );
