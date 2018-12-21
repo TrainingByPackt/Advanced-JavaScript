ef0dfb052f569da0f072874ba2bd01e03cafd42f
Zach Shute <cosmonot1@gmail.com>
2018-12-21 13:36:57 -0800

    update exercises

--- exercises/exercise18/index.js
+++ exercises/exercise18/index.js
@@ -1,13 +1,5 @@
 'use strict';
 
-/* Assignment:
-
-Your team is building an API that is based around callbacks. To prevent runtime errors, you need to validate that the callback argument passed into the callback API funcatiobs are valid callable functions. Create a function for your API. In the body of that function, validate that the callback argument is a function. If it is not a function, throw an error. After a delay, log the data passed into the API function and call the callback.
-
- */
-
-// Solution:
-
 function higherOrder( data, cb ) {
   if ( !cb || !( typeof cb === 'function' ) ) {
     throw new Error( 'Invalid callback. Please provide a function.' );
@@ -28,12 +20,3 @@ try {
 higherOrder( 1, () => {
   console.log( 'Callback Called!' )
 } );
-
-/* Expected output
-
-Got error: Invalid callback. Please provide a function.
-1
-Callback Called!
-
- */
-// Note: student solutions may vary slightly depending on their input
--- exercises/exercise20/index.js
+++ exercises/exercise20/index.js
@@ -1,20 +1,10 @@
 'use strict';
 
-/* Assignment:
-
-You are building a promise based API. In your API, you must validate user input to ensure data passed into your database models is of the right type. Write a function that returns a promise. This promise should validate that the data value passed into the API function is not a number. If the user passes a number into the function, reject the promise with an error. If the user passes a non number into the API function, resolve the promise with the word 'Success!'
-
-
- */
-
-// Solution:
-
 function promiseFunction( data ) {
   return new Promise( ( resolve, reject ) => {
     setTimeout( () => {
 
       console.log( data );
-
 
       if ( typeof data === 'number' ) {
         return reject( new Error( 'Data cannot be of type \'number\'.' ) );
@@ -28,11 +18,3 @@ function promiseFunction( data ) {
 
 promiseFunction( 1 ).then( console.log ).catch( err => console.log( `Error: ${err.message}` ) );
 promiseFunction( 'test' ).then( console.log ).catch( err => console.log( `Error: ${err.message}` ) );
-
-/* Expected output;
-
-1
-Error: Data cannot be of type 'number'.
-test
-Success!
-*/
