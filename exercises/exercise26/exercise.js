efa28f9ee882e477389d2d80389af937edcdd8f9
Zach Shute <cosmonot1@gmail.com>
2018-12-21 16:06:40 -0800

    fix ordering

--- exercises/exercise26/exercise.js
+++ exercises/exercise26/exercise.js
@@ -1,8 +1,8 @@
 'use strict';
 
-function reverseRounding( arg1 ) {
-  const decimal = arg1 % 1;
-  if ( decimal >= 0.5 ) {
+function fakeRounding( arg1 ) {
+  const decimal = Math.abs(arg1) % 1;
+  if ( decimal <= 0.5 ) {
     return Math.ceil( arg1 );
   }
   return Math.floor( arg1 );
--- exercises/exercise29/index.js
+++ exercises/exercise29/index.js
@@ -1,10 +1,26 @@
 'use strict';
 
-const data = [ { f1: 6, f2: 3 }, { f1: 12, f2: 0 }, { f1: 9, f2: 1 }, { f1: 6, f2: 7 } ];
-function swap( key1, key2 ) {
-  return obj => {
-    [ obj[ key1 ], obj[ key2 ] ] = [ obj[ key2 ], obj[ key1 ] ];
-    return obj;
+// Activity B:
+// Your research team has obtained a list of value from your latest experiment however due to a calibration error only some of the data can be used and any data that can be used needs to be scaled. You must to build a utility function that takes in an array, filters out any values less than or equal to 0, scales the remaining values by multiplying them by 2, and returns the final results. First build an imperative function to do this, then build a declarative function to do the same thing.
+
+function imperative( arr ) {
+  const filtered = [];
+  for ( let i = 0; i < arr.length; i++ ) {
+    if ( arr[ i ] > 0 ) {
+      filtered.push( arr[ i ] );
+    }
   }
+  for ( let j = 0; j < filtered.length; j++ ) {
+    filtered[ j ] = 2 * filtered[ j ];
+  }
+  return filtered;
 }
-console.log( data.map( swap( 'f1', 'f2' ) ) );
+
+function declarative( arr ) {
+  return arr.filter( v => v > 0 ).map( v => 2 * v );
+}
+
+//Test values
+const values = [ -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 ];
+console.log( imperative( values ) ); // Output: [ 2, 4, 6, 8, 10 ]
+console.log( declarative( values ) ); // Output: [ 2, 4, 6, 8, 10 ]
