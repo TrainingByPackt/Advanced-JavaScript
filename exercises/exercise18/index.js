55019c1017f1cf25078fa10882f4d4c285b6effe
Zach Shute <cosmonot1@gmail.com>
2019-01-03 18:33:46 -0800

    update numbering

--- exercises/exercise18/index.js
+++ exercises/exercise18/index.js
@@ -1,5 +1,22 @@
-const myPromise = new Promise( ( resolve, reject ) => {
-  console.log( 'Starting asynchronous work!' );
-  setTimeout( () => { resolve( 'Done!' ); }, 1000 );
+'use strict';
+
+function higherOrder( data, cb ) {
+  if ( !cb || !( typeof cb === 'function' ) ) {
+    throw new Error( 'Invalid callback. Please provide a function.' );
+  }
+
+  console.log( data );
+
+  setTimeout( cb, 10 );
+
+}
+
+try {
+  higherOrder( 1, null );
+} catch ( err ) {
+  console.log( `Got error: ${err.message}` );
+}
+
+higherOrder( 1, () => {
+  console.log( 'Callback Called!' )
 } );
-myPromise.then( value => console.log( value ) );
--- exercises/exercise19/index.js
+++ exercises/exercise19/index.js
@@ -1,20 +1,5 @@
-'use strict';
-
-function promiseFunction( data ) {
-  return new Promise( ( resolve, reject ) => {
-    setTimeout( () => {
-
-      console.log( data );
-
-      if ( typeof data === 'number' ) {
-        return reject( new Error( 'Data cannot be of type \'number\'.' ) );
-      }
-
-      resolve( 'Success!' );
-
-    }, 10 );
-  } );
-}
-
-promiseFunction( 1 ).then( console.log ).catch( err => console.log( `Error: ${err.message}` ) );
-promiseFunction( 'test' ).then( console.log ).catch( err => console.log( `Error: ${err.message}` ) );
+const myPromise = new Promise( ( resolve, reject ) => {
+  console.log( 'Starting asynchronous work!' );
+  setTimeout( () => { resolve( 'Done!' ); }, 1000 );
+} );
+myPromise.then( value => console.log( value ) );
