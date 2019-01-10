a47315f2542c0b2fe5033b25ad2e58c445578c0c
Zach Shute <cosmonot1@gmail.com>
2019-01-10 09:18:50 -0800

    update names

--- exercises/exercise35/index.js
+++ exercises/exercise35/index.js
@@ -1,9 +1,5 @@
 'use strict';
 
-/*
-  Activity: You have been tasked to build a NodeJS express server for a note taking application. The server should serve a basic HTML page (provided in the activity folder under index.html) to the base route ('/'). The server will need to have an API route to load a saved note from a text file in the server's local file system and an API route to save changes to the note to the text file on the server's local file system. The server should accept a GET request to the URI '/load' when loading the note, and accept a POST request to the URI '/save' when saving the note. The provided HTML file will assume that these are the API paths used on the server. When building the server, you may want to use the body-parser middleware with the option strict set to false to simplify the handling of request.
- */
-
 const express = require( 'express' );
 const http = require( 'http' );
 const bodyParser = require( 'body-parser' );
