46ba7e9ca252ddfc7b1784e9dc7d7fb183cb712c
Zach-PC\Zach <cosmonot1@gmail.com>
2019-01-28 19:59:44 -0800

    fibonacci fix

--- activities/activity4/test.js
+++ activities/activity4/test.js
@@ -4,7 +4,7 @@ const { fibonacci } = require( './index.js' );
 
 describe( 'fibonacci', () => {
 
-  it( 'n=0 should equal 1', ( done ) => {
+  it( 'n=0 should equal 0', ( done ) => {
     done( fibonacci( 0 ) !== 0 );
   } );
 
@@ -12,19 +12,19 @@ describe( 'fibonacci', () => {
     done( fibonacci( 1 ) !== 1 );
   } );
 
-  it( 'n=2 should equal 2', ( done ) => {
+  it( 'n=2 should equal 1', ( done ) => {
     done( fibonacci( 2 ) !== 1 );
   } );
 
-  it( 'n=5 should equal 8', ( done ) => {
+  it( 'n=5 should equal 5', ( done ) => {
     done( fibonacci( 5 ) !== 5 );
   } );
 
-  it( 'n=7 should equal 21', ( done ) => {
+  it( 'n=7 should equal 13', ( done ) => {
     done( fibonacci( 7 ) !== 13 );
   } );
 
-  it( 'n=9 should equal 55', ( done ) => {
+  it( 'n=9 should equal 34', ( done ) => {
     done( fibonacci( 9 ) !== 34 );
   } );
 
